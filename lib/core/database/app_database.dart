8d1a610500088529d2ad2eac02fa7c712703a52a
Nelson Camungondo <ncamungondo@gmail.com>
2026-04-18 06:35:26 +0100

    Configure GitHub Pages deploy and Drift Web support

--- lib/core/database/app_database.dart
+++ lib/core/database/app_database.dart
@@ -1,9 +1,5 @@
-import 'dart:io';
-
 import 'package:drift/drift.dart';
-import 'package:drift/native.dart';
-import 'package:path/path.dart' as p;
-import 'package:path_provider/path_provider.dart';
+import 'package:drift_flutter/drift_flutter.dart';
 
 part 'app_database.g.dart';
 
@@ -34,12 +30,8 @@ class PalavraDicionarios extends Table {
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(_openConnection());
 
-  static LazyDatabase _openConnection() {
-    return LazyDatabase(() async {
-      final dir = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dir.path, 'harpa_crista_angolana.db'));
-      return NativeDatabase(file);
-    });
+  static QueryExecutor _openConnection() {
+    return driftDatabase(name: 'harpa_crista_angolana');
   }
 
   @override
